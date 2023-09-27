17e727198e268608af7594cb8a88488e9faf1f1b
KristoCsongor <kristolcsongor@uni.sapientia.ro>
2023-09-27 09:35:21 +0300

    some comments

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
   const [prevGames, setPrevGames] = useState([]);
 
   const gameDataHandler = (coordData) => {
-    let gameIndex = prevGames.length + 1;
-    prevGames.push({...coordData, gameIndex});
+    let gameIndex = prevGames.length + 1; // for dynamic data - GameDetailPage
+    prevGames.push({...coordData, gameIndex}); // list of all previous games - PrevGamesPage
     console.log(coordData);
     console.log(prevGames);
 
-    localStorage.setItem("games", JSON.stringify(prevGames));
+    localStorage.setItem("games", JSON.stringify(prevGames)); // temporarily storing previous games
   };
 
   const router = createBrowserRouter([
@@ -30,9 +30,9 @@ function App() {
         { index: true, element: <HomePage /> },
         {
           path: "start",
-          element: <StartGamePage onFinalisedGame={gameDataHandler} />,
+          element: <StartGamePage onFinalisedGame={gameDataHandler} />, // getting the data from the game page
         },
-        { path: "games", element: <PrevGamesPage prevGamesData={prevGames} /> },
+        { path: "games", element: <PrevGamesPage prevGamesData={prevGames} /> }, // sending down the data to previous games page
         { path: "games/:gameId", element: <GameDetailPage /> },
       ],
     },
